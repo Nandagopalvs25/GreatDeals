fe183bae619876ef211157be3ec4d6a2a658e637
snape <nandagopalvs12@gmail.com>
2021-10-13 18:52:46 +0530

    title changed.

--- src/main/java/com/example/webscrap/HelloApplication.java
+++ src/main/java/com/example/webscrap/HelloApplication.java
@@ -18,7 +18,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Great Deals!");
         stage.setScene(scene);
 
         stage.show();
